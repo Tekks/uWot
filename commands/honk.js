559caf21aaee7850126208fa8ba1d97dc5c45ad0
Andy <andreas-heil@hotmail.de>
2020-10-06 18:23:40 +0000

    added: honk dm message for LaMetric

--- commands/honk.js
+++ commands/honk.js
@@ -3,10 +3,9 @@ const VoiceUtil = require('../handler/voice.js')
 module.exports = {
     name: ['honk', 'airhorn'],
     description: 'Honks at People',
-    execute(message, args) {
-        let userId = message.author.id
+    execute(client, message, args) {
         const voiceUtil = new VoiceUtil()
-        let voiceChannel = voiceUtil.getVoiceChannel(message.client, userId)
+        let voiceChannel = voiceUtil.getVoiceChannel(client, message.author.id)
         if (voiceChannel) {
             voiceUtil.connectVoiceChannel(voiceChannel)
                 .then(() => voiceUtil.playAudio('/var/www/tks.wtf/public/sounds/airhorn.mp3', true))
