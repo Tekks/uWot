8ba3996a243d341dd3390f5b797004f7e8d7f5b3
Andy <andreas-heil@hotmail.de>
2020-10-20 02:51:33 +0000

    message delete countdown and errorhandling

--- commands/_honk.js
+++ commands/_honk.js
@@ -10,10 +10,22 @@ module.exports = {
         let voiceChannel = voiceUtil.getVoiceChannel(client, message.author.id)
         if (voiceChannel) {
             voiceUtil.connectVoiceChannel(voiceChannel)
-                .then(() => voiceUtil.playAudio('/var/www/tks.wtf/public/sounds/airhorn.mp3', true))
+                .then(() => voiceUtil.playAudio('/var/www/tks.wtf/public/sounds/airhorn.mp3', true)).catch(
+                    (e) => message.reply(`<:uwot_pepega:767928479287083050> 📣 ${e}`)
+                )
+        } else {
+            message.reply("<:uwot_pepega:767928479287083050> 📣 you are not in an watched VC")
         }
-        if (message.channel.type == "text"){
-            try { message.delete() }catch{}
+        if (message.channel.type == "text") {
+            try {
+                //message.delete() 
+                message.react('🗑️')
+                message.react('3️⃣')
+                message.react('2️⃣')
+                message.react('1️⃣')
+                message.react('0️⃣').then(() => message.delete())
+
+            } catch { }
         }
     },
 }
