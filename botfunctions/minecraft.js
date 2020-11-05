ade27098801fde0270be3b23c089124d02ae32a1
Tekks <nottekks@hotmail.com>
2020-11-05 18:22:26 +0100

    fixed mc status errors

--- botfunctions/minecraft.js
+++ botfunctions/minecraft.js
@@ -38,29 +38,27 @@ class Minecraft {
                 .setTitle(`Server Players ( ${that.serverAddress} )`)
                 .setAuthor('uWot Bot', 'https://cdn.discordapp.com/avatars/755913893062246511/2d6c33c51b36565a2f57ad1a1fc508b1.png', 'https://tks.wtf/#uwot')
                 .setThumbnail('https://i1.wp.com/www.craftycreations.net/wp-content/uploads/2019/08/Grass-Block-e1566147655539.png')
-            mc.queryFull(that.serverAddress)
+            mc.status(that.serverAddress)
                 .then((response) => {
                     var i = 1
-                    embed.setDescription('```' + response.onlinePlayers +  ' of ' + response.maxPlayers + ' Players online```')
-                        .setColor('#00ff00')
-                    if (that.getPlayerMemory() == response.players) { that.setPlayerMemory(response.players.join(',')); return }
-                    that.setPlayerMemory(response.players.join(','))
-                    response.players.forEach(element => {
-                        embed.addField(`Player ${i}`, element, true)
-                        i++
-                    });
+                    embed.setDescription('```' + response.onlinePlayers + ' of ' + response.maxPlayers + ' Players online```').setColor('#00ff00')
+                    if (response.samplePlayers === that.getPlayerMemory()) { return } else { that.setPlayerMemory(response.samplePlayers) }
+                    if (response.samplePlayers != null) {
+                        response.samplePlayers.forEach(player => { embed.addField(`Player ${i}`, player.name, true); i++ });
+                    }
                     that.client.channels.cache.get(that.channelId).messages.fetch(that.messageId)
                         .then(message => message.edit(embed))
                 })
-                .catch((error) => {
-                    console.log(error)
-                    embed.setDescription('```Server offline```')
-                        .setColor('#ff0000')
+                .catch(() => {
+                    embed.setDescription('```Server offline```').setColor('#ff0000')
                     that.client.channels.cache.get(that.channelId).messages.fetch(that.messageId)
                         .then(message => message.edit(embed))
-                })
-        }, 30000)
+                })    
+        }, 5000)
+
+
     }
+
 
     changeChannelName() {
         var that = this
